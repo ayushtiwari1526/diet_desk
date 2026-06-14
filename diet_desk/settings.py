a4dcc62f59429b43455952b00f08eb277b6c7797
ayushtiwari1526 <ayushtiwarirs45@gmail.com>
2026-06-14 08:45:17 +0530

    Fix media serving configuration

--- diet_desk/settings.py
+++ diet_desk/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x)tsrh+tmbh_z6i15qi#c_e#(@tpdk1dc&#$im=@f3%h1bgckf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 #ALLOWED_HOSTS = []
 
